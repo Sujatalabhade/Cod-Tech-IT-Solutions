f9dd5dc5d33ff982ed08b8b5499b8f36bc932bcc
Sujatalabhade <156834136+Sujatalabhade@users.noreply.github.com>
2024-01-25 22:14:42 +0530

    Add files via upload

---
+++ Task2/BasicCalculator.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1, num2, result;
+        char operator;
+
+        System.out.print("Enter First Number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Please Choose Operation which you want to perform,  (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter Second Number: ");
+        num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid Operator.");
+        }
+
+        scanner.close();
+    }
+}
